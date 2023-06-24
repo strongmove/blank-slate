9f37c9e0627d830721e93a3f0b19d8b450a5f56b
admin admin <admin@admin.admin>
2023-06-23 21:18:35 -0700

    Init
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .

---
+++ create-next-app.py
@@ -0,0 +1,55 @@
+# python (wget -qO - https://raw.githubusercontent.com/strongmove/blank-slate/main/create-next-app.py | psub)
+
+from subprocess import run
+
+DEFI = {
+    "nextjs": {
+        "prompt": {"type": "confirm", "name": "nextjs", "default": "y"},
+        "actions": [lambda: "pnpm create next-app --ts ."],
+    },
+    "xstate": {
+        "prompt": {"type": "confirm", "name": "xstate", "default": "y"},
+        "actions": [lambda: "pnpm add xstate @xstate/react"],
+    },
+    "shadcn-ui@latest": {
+        "prompt": {"type": "confirm", "name": "shadcn-ui@latest", "default": "y"},
+        "actions": [
+            lambda: "pnpm dlx shadcn-ui@latest init",
+            lambda: "pnpm dlx shadcn-ui@latest add",
+        ],
+    },
+    "sass": {
+        "prompt": {"type": "confirm", "name": "sass", "default": "y"},
+        "actions": [lambda: "pnpm add -D sass"],
+    },
+    "zod": {
+        "prompt": {"type": "confirm", "name": "zod", "default": "y"},
+        "actions": [lambda: "pnpm add zod"],
+    },
+    "react-icons": {
+        "prompt": {"type": "confirm", "name": "react-icons", "default": "y"},
+        "actions": [lambda: "pnpm add react-icons"],
+    },
+}
+
+
+def run_actions(actions):
+    for action in actions:
+        if type(action) == list:
+            run_actions(action)
+        else:
+            run(action(), shell=True)
+
+
+def main():
+    actions = []
+    for name, defi in DEFI.items():
+        choice = input(f"{name} (Y/n): ")
+        if not choice:
+            choice = defi["prompt"]["default"]
+        if choice.lower() == "y":
+            actions += [defi["actions"]]
+    run_actions(actions)
+
+
+main()
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -i
+
+# wget -O /tmp/blank-slate.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install.sh && bash /tmp/blank-slate.sh
+
+sudo apt update -y
+sudo apt install curl build-essential git -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc
+
+/home/linuxbrew/.linuxbrew/bin/brew install fish
+command -v fish | sudo tee -a /etc/shells
+chsh -s "$(command -v fish)"
+
+/home/linuxbrew/.linuxbrew/bin/brew install chezmoi
+/home/linuxbrew/.linuxbrew/bin/chezmoi init git@github.com:strongmove/dotfiles.git
+/home/linuxbrew/.linuxbrew/bin/chezmoi update
+/home/linuxbrew/.linuxbrew/bin/fish
