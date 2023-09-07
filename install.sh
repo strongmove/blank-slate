13905a8fb8fd12ef9cf8554d92c29bf46b2bfe11
Strongmove <strongmove@localhost>
2023-09-07 12:43:01 -0700

    .

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 source ~/.bashrc
 
 # Essentials
-/home/linuxbrew/.linuxbrew/bin/brew install fish chezmoi diff-so-fancy
+/home/linuxbrew/.linuxbrew/bin/brew install fish
+# /home/linuxbrew/.linuxbrew/bin/brew install fish chezmoi diff-so-fancy
 # Extras
 # /home/linuxbrew/.linuxbrew/bin/brew install exa fzf ripgrep zoxide tmux xsel lazygit zk
 # Setup fish as default shell
 command -v fish | sudo tee -a /etc/shells
 chsh -s "$(command -v fish)"
 
-/home/linuxbrew/.linuxbrew/bin/chezmoi init git@github.com:strongmove/dotfiles.git
-/home/linuxbrew/.linuxbrew/bin/chezmoi update
-/home/linuxbrew/.linuxbrew/bin/fish
+# /home/linuxbrew/.linuxbrew/bin/chezmoi init git@github.com:strongmove/dotfiles.git
+# /home/linuxbrew/.linuxbrew/bin/chezmoi update
+# /home/linuxbrew/.linuxbrew/bin/fish
