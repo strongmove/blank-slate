bd5ea611696d185fb613e7a63c3954a3acdd92ee
Strongmove <strongmove@localhost>
2023-09-07 10:55:34 -0700

    .

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -i
+#!/bin/bash -ilvx
 
 # wget -O /tmp/blank-slate.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install.sh && bash /tmp/blank-slate.sh
 
@@ -9,10 +9,10 @@ sudo apt install curl build-essential git -y
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 source ~/.bashrc
 
-/home/linuxbrew/.linuxbrew/bin/brew install fish chezmoi diff-so-fancy
+/home/linuxbrew/.linuxbrew/bin/brew install fish chezmoi diff-so-fancy exa fzf ripgrep zoxide tmux xsel lazygit zk
 command -v fish | sudo tee -a /etc/shells
 chsh -s "$(command -v fish)"
 
 /home/linuxbrew/.linuxbrew/bin/chezmoi init git@github.com:strongmove/dotfiles.git
 /home/linuxbrew/.linuxbrew/bin/chezmoi update
-/home/linuxbrew/.linuxbrew/bin/fish
+# /home/linuxbrew/.linuxbrew/bin/fish
