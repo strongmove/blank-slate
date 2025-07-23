ca4b49c9218ec58a7524026984cb36053c344617
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-22 17:02:21 -0700

    Update install_v2.sh

--- install_v2.sh
+++ install_v2.sh
@@ -18,7 +18,7 @@ git -C ~/.config/fish pull
 # Essentials
 $BREWBIN/brew install chezmoi diff-so-fancy
 # Extras
-$BREWBIN/brew install eza fzf ripgrep zoxide tmux xsel lazygit
+$BREWBIN/brew install eza fzf ripgrep fd zoxide tmux xsel lazygit
 $BREWBIN/brew install zk rust bat gdu bpytop ranger
 # Setup fish as default shell
 grep -qF fish /etc/shells || command -v $BREWBIN/fish | sudo tee -a /etc/shells
