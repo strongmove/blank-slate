987910e576cc3faf951c6ea5776a6c6356eed61e
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-24 00:22:07 -0700

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ilvx
 
+# On a new machine running live image, run `archinstall` and reboot before continuing
+
+
 # curl -fsSL https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh | bash
+
 
 sudo pacman -Syu --noconfirm curl git python-pipx unzip dnsutils fish chezmoi diff-so-fancy eza fzf ripgrep zoxide tmux xsel lazygit fd zk rust bat gdu bpytop ranger
 
