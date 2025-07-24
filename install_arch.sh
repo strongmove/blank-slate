58ea12fa5e9dd909e933280e27f0ff40c5b127cb
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-24 00:40:45 -0700

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -5,14 +5,14 @@
 # 2) import $HOME/.ssh to allow git clone from private github repository.
 
 
-# curl -o script.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh && bash script.sh
+curl -o script.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh && bash script.sh
 
-# sudo pacman -Syu --noconfirm curl git python-pipx unzip dnsutils fish chezmoi diff-so-fancy eza fzf ripgrep zoxide tmux xsel lazygit fd zk rust bat gdu bpytop ranger
+sudo pacman -Syu --noconfirm curl git python-pipx unzip dnsutils fish chezmoi diff-so-fancy eza fzf ripgrep zoxide tmux xsel lazygit fd zk rust bat gdu bpytop ranger
 
-# git clone git@github.com:strongmove/fish "$HOME/.config/fish"
+git clone git@github.com:strongmove/fish "$HOME/.config/fish"
 
-#chezmoi init git@github.com:strongmove/dotfiles.git
-#chezmoi update
+chezmoi init git@github.com:strongmove/dotfiles.git
+chezmoi update
 
 chsh -s /usr/bin/fish
 /usr/bin/fish
