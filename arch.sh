8e55756f979e22c882ead3deedba338f9cc58868
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-24 02:15:14 -0700

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ilvx
+
+# On a new machine running live image
+# 1) run `archinstall` and reboot before continuing
+# 2) import $HOME/.ssh to allow git clone from private github repository.
+
+# curl -o script.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/arch.sh && bash script.sh
+
+
+if [ ! -d "$HOME/.ssh" ]; then
+  echo "Cannot continue as user $USER because $HOME/.ssh does not exist."
+  echo "Parts of this setup requires accessing private Github repositories."
+  echo "Please install appropriate ssh keys into $HOME/.ssh and try again."
+  exit 1
+fi
+
+sudo pacman -Syu --noconfirm curl git fish
+
+git clone git@github.com:strongmove/fish "$HOME/.config/fish"
+
+chezmoi init git@github.com:strongmove/dotfiles.git
+chezmoi update
+
+chsh -s /usr/bin/fish
+/usr/bin/fish
