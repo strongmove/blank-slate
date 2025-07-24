f670a2eab268fb2135c84e94ff30d8b48a5b0398
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-23 23:44:06 -0700

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,1 +1,13 @@
-pacman -Syu unzip
+#!/bin/bash -ilvx
+
+# wget -O a.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh && bash a.sh
+
+sudo pacman -Syu --noconfirm curl git python-pipx unzip dnsutils fish chezmoi diff-so-fancy eza fzf ripgrep zoxide tmux xsel lazygit fd zk rust bat gdu bpytop ranger
+
+git clone git@github.com:strongmove/fish "$HOME/.config/fish"
+
+chsh -s /usr/bin/fish
+
+chezmoi init git@github.com:strongmove/dotfiles.git
+chezmoi update
+/usr/bin/fish
