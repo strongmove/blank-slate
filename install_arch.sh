e74613d35344ede13e907e99cb831a5c0e47893e
strongmove <94773539+strongmove@users.noreply.github.com>
2025-07-24 00:42:05 -0700

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -4,8 +4,14 @@
 # 1) run `archinstall` and reboot before continuing
 # 2) import $HOME/.ssh to allow git clone from private github repository.
 
+# curl -o script.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh && bash script.sh
 
-curl -o script.sh https://raw.githubusercontent.com/strongmove/blank-slate/main/install_arch.sh && bash script.sh
+if [ ! -d "$HOME/.ssh" ]; then
+  echo "Cannot continue as user $USER because $HOME/.ssh does not exist."
+  echo "Parts of this setup requires accessing private Github repositories."
+  echo "Please install appropriate ssh keys into $HOME/.ssh and try again."
+  exit 1
+fi
 
 sudo pacman -Syu --noconfirm curl git python-pipx unzip dnsutils fish chezmoi diff-so-fancy eza fzf ripgrep zoxide tmux xsel lazygit fd zk rust bat gdu bpytop ranger
 
